ff58aa16c23131f69c7ce6fd90e1cb755eb4b870
Manish Singh <singhmanish.personal@gmail.com>
2020-10-10 14:43:55 +0530

    Ping Service

---
+++ Ping_Service/config.js
@@ -0,0 +1,17 @@
+var env={};
+
+env.stage ={
+
+    'port':3000,
+    'envName':'stage'
+}
+
+env.prod ={
+
+    'port':5000,
+    'envName':'prod'
+}
+
+var currEnv= typeof(process.env.NODE_ENV)=='string' ? env[process.env.NODE_ENV.toLowerCase()]:env['stage'];
+
+module.exports=currEnv;
---
+++ Ping_Service/index.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var config=require('./config')
+var url=require('url');
+const { callbackify } = require('util');
+
+
+
+var httpServer=http.createServer(function(req,res){
+var parseUrl=url.parse(req.url);
+
+var pathName=parseUrl.pathname;
+var trimmedPath = pathName.replace(/^\/+|\/+$/g, '');
+
+var chooseHandler=typeof(router[trimmedPath])!=='undefined'? router[trimmedPath] : handlers.notFound;
+
+
+chooseHandler(function(statusCode,payload){
+
+    res.setHeader('Content-Type','application/json');
+    res.writeHead(statusCode);
+    res.end(JSON.stringify(payload));
+    console.log(trimmedPath,statusCode);
+
+
+
+});
+
+
+});
+
+httpServer.listen(config.port, function(){
+console.log("Listening on "+config.port+"...");
+
+});
+
+var handlers={};
+
+handlers.hello = function(callback){
+    
+    callback(200,{'message':'Welcome'})
+};
+
+ handlers.notFound = function(callback){
+     callback(404,{'message':'Not found'})
+};
+var router={
+
+    'hello':handlers.hello
+
+};
